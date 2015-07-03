063b97991e32772cafaffec43e4b079b3120317b
barbelith <pedropalmero@gmail.com>
2015-07-03 17:00:23 +0200

    Add Folder references to a target

--- Source/XCSourceFile.m
+++ Source/XCSourceFile.m
@@ -114,7 +114,7 @@
 
 - (BOOL)canBecomeBuildFile
 {
-    return _type == SourceCodeObjC || _type == SourceCodeObjCPlusPlus || _type == SourceCodeCPlusPlus || _type == XibFile || _type == Framework || _type == ImageResourcePNG || _type == HTML || _type == Bundle || _type == Archive || _type == AssetCatalog;
+    return _type == SourceCodeObjC || _type == SourceCodeObjCPlusPlus || _type == SourceCodeCPlusPlus || _type == XibFile || _type == Framework || _type == ImageResourcePNG || _type == HTML || _type == Bundle || _type == Archive || _type == AssetCatalog || _type == Folder;
 }
 
 
@@ -126,7 +126,7 @@
     else if (_type == Framework) {
         return PBXFrameworksBuildPhaseType;
     }
-    else if (_type == ImageResourcePNG || _type == HTML || _type == Bundle || _type == AssetCatalog) {
+    else if (_type == ImageResourcePNG || _type == HTML || _type == Bundle || _type == AssetCatalog || _type == AssetCatalog) {
         return PBXResourcesBuildPhaseType;
     }
     else if (_type == Archive) {
